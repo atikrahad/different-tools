4aec59c7108046f55dcec453b162d8d86ccedc36
Atik Rahad <atikrahad1@gmail.com>
2023-08-03 15:07:47 +0600

    adding mile to kilomiter

--- js/main.js
+++ js/main.js
@@ -9,7 +9,24 @@ function getFeet(e){
     e.preventDefault()
     
         let feetans = userInche.value / 12;
-        feetEl.innerHTML = feetans;
+        feetEl.innerHTML = feetans.toFixed(2);
+    
+
+}
+
+
+const mileTokilo = document.querySelector("#mTk");
+const userMileEl = document.querySelector("#userMile");
+const kiloEl = document.querySelector("#kilo");
+
+mileTokilo.addEventListener("submit", getKilo)
+
+
+function getKilo(e){
+    e.preventDefault()
+    
+        let kilomiters = userMileEl.value * 1.60934;
+        kiloEl.innerHTML = kilomiters.toFixed(2);
     
 
 }
